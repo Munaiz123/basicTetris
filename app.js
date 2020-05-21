74eb7bea7379acf86a989393b3a20e9be4bfe141
Munaiz <munaizahmed2@gmail.com>
2020-05-21 15:48:55 -0500

    first commit - following Tetris Tutorial to brush up on DOM manipulation basics

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll(".grid div"))
+  const scoreDisplay = document.getElementById('score')
+  const startButton = document.getElementById('start-button')
+  const width = 10
+
+  console.log("squares", squares);
+  console.log('scoreDisplay', scoreDisplay);
+  console.log('startButton', startButton);
+
+
+})
