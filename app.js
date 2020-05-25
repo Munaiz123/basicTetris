8c2c172298d887b7773403c8ad691b3692ac9030
Munaiz <munaizahmed2@gmail.com>
2020-05-25 16:27:01 -0500

    random shapes are appearing on gameboard on page refresh

--- app.js
+++ app.js
@@ -5,40 +5,62 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const startButton = document.getElementById('start-button')
   const width = 10
 
-  const lTet = [
+  const lShape = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2],
   ];
 
-  const zTet = [
+  const zShape = [
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
     [0, width, width + 1, width * 2 + 1],
     [width + 1, width + 2, width * 2, width * 2 + 1],
   ];
 
-  const tTet = [
+  const tShape = [
     [1, width, width + 1, width + 2],
     [1, width + 2, width + 2, width * 2 + 1],
     [width, width + 1, width + 2, width * 2 + 1],
     [1, width, width + 1, width * 2 + 1],
   ];
 
-  const oTet = [
+  const oShape = [
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
     [0, 1, width, width + 1],
   ];
 
-  const iTet = [
+  const iShape = [
     [1, width, +1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
   ];
 
+  const shapes = [lShape,zShape,tShape,oShape,iShape]
 
+  let currentPosition = 4
+  let currentRotation = 0
+
+  // select shape at random
+  let random = Math.floor(Math.random()*shapes.length)
+  let current = shapes[random][0]
+
+
+  function draw(){
+    current.forEach(index =>{
+      squares[currentPosition + index].classList.add('tetrimino')
+    })
+  }
+
+  function undraw(){
+    current.forEach(index =>{
+      squares[currentPosition + index].classList.remove('tetrimino')
+    })
+  }
+
+  draw();
 })
