3930adc09989f2ac296392ba2d476df983eb9b04
Munaiz <munaizahmed2@gmail.com>
2020-05-25 16:10:17 -0500

    added tetriminoes

--- app.js
+++ app.js
@@ -5,9 +5,40 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const startButton = document.getElementById('start-button')
   const width = 10
 
-  console.log("squares", squares);
-  console.log('scoreDisplay', scoreDisplay);
-  console.log('startButton', startButton);
+  const lTet = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
+
+  const zTet = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
+
+  const tTet = [
+    [1, width, width + 1, width + 2],
+    [1, width + 2, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
+
+  const oTet = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+
+  const iTet = [
+    [1, width, +1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
 
 
 })
